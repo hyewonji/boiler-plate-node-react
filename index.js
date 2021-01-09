05097d959a2ea4f850eebe7ba07fee7df04767ef
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-01-09 23:30:14 +0900

    Connect MongoDB

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://hye1:086975864@boiler-plate-node-react.6h4is.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost: ${port}`)
+}) 
