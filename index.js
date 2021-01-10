d945558d2126f94a81d4abee78538667245f794e
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-01-10 16:17:13 +0900

    Download PostMan, Implement Signup Function Using BodyParser

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://hye1:086975864@boiler-plate-node-react.6h4is.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -8,9 +16,20 @@ mongoose.connect('mongodb+srv://hye1:086975864@boiler-plate-node-react.6h4is.mon
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) =>  res.send('Hello World!'))
+
+app.post('/register', (req,res) => {
+
+  const user = new User(req.body) //bodyParser로 body에 있는 회원정보를 가져옴
+  
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost: ${port}`)
