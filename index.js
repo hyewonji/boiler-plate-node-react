62d1b1bfe1add4ceb3691f094554c66950d8a308
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-01-11 23:25:24 +0900

    Install Nodemon npm install nodemon --save-dev로 package.json에devDependencies추가, script에 nodemon index.js를 추가해 서버를 껐다 키지 않아도 작동하게 설정

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 
-//application/x-www-form-urlencoded
+// application/x-www-form-urlencoded
+// bodyParser.urlencoded() -> 자동으로 req에 body속성이 추가되고 저장
+// extended: true -> 중첩된 객체 표현여부 결정(객체안에 객체 파싱 가능하게 한다.)
 app.use(bodyParser.urlencoded({extended:true}));
 
-//application/json
+// application/json
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
